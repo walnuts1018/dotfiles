f62f01dd7f7a785298a60f5a9035dd8e832a3a18
walnuts1018 <r.juglans.1018@gmail.com>
2024-09-02 17:56:24 +0900

    fix
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- setup.sh
+++ setup.sh
@@ -16,13 +16,12 @@ else
 fi
 
 if [ "${OS}" == 'Linux' ]; then
-    {{ type apt >/dev/null 2>&1 }} && sudo apt install -y zsh git
-    echo "$password" | sudo -S chsh -s $(which zsh)
-    [[ ! -s "$HOME/.zim" ]] && curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+  {{ type apt }} >/dev/null 2>&1 && sudo apt install -y zsh git
+  echo "$password" | sudo -S chsh -s $(which zsh)
+  [[ ! -s "$HOME/.zim" ]] && curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 
-    sh -c "$(curl -fsLS get.chezmoi.io)" && echo "$password" | sudo -S mv $HOME/bin/chezmoi /usr/local/bin
-    chezmoi init --apply https://github.com/walnuts1018/dotfiles.git
+  sh -c "$(curl -fsLS get.chezmoi.io)" && echo "$password" | sudo -S mv $HOME/bin/chezmoi /usr/local/bin
+  chezmoi init --apply https://github.com/walnuts1018/dotfiles.git
 
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
